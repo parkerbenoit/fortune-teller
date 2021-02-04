21e4a0140e55bf55507f30cdfc8073fe3dece0de
parkerbenoit <72704200+parkerbenoit@users.noreply.github.com>
2021-02-03 20:40:31 -0800

    broken code :D

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
+
+
 $(document).ready(function() {
   $("form#fortuneTeller").submit(function(event) {
     event.preventDefault();
-    
-  }
-}
+    $("input:checkbox[name=fortune-questions]:checked").each(function() {
+      const fortuneQuestions = $(this).val();
+      console.log(fortuneQuestions); 
+      
+    })
+  });
+});
